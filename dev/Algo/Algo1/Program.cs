43f46977d6467d115b8b5801e64f1e145228481a
Ali MAKRI <github@makri.fr>
2023-03-20 15:57:37 +0100

    16:00

---
+++ dev/Algo/Algo1/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Deviner un nombre compris entre 1 et 99");
+Random alea = new Random();
+int nombreADeviner = alea.Next(1, 100);
+//Console.WriteLine(nombreADeviner);
+
+int proposition = 0;
+int nCoup = 0;
+while (proposition != nombreADeviner)
+{
+    Console.Write("Proposition : ");
+    var propositionStr = Console.ReadLine();
+    nCoup++;
+    if (int.TryParse(propositionStr, out proposition))
+    {
+        if (proposition < nombreADeviner)
+        {
+            Console.WriteLine("Trop petit");
+        }
+        else if (proposition > nombreADeviner)
+        {
+            Console.WriteLine("Trop grand");
+        }
+        else
+        {
+            Console.WriteLine("Gagné");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Nombre incorrect");
+    }
+}
+Console.ReadLine();
