a0c8596778bacd994cbcdf4e228512988b44a125
Ali MAKRI <github@makri.fr>
2023-03-21 14:17:38 +0100

    14:17

--- dev/Algo/Algo5/Program.cs
+++ dev/Algo/Algo5/Program.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,33 +11,48 @@ namespace Algo5
     {
         static void Main(string[] args)
         {
-            // Partie 1 : saisie
+            // Partie 0 : lecture du fichier
             var personnes = new Dictionary<string, string>();
-            while(true)
+            var lignes = File.ReadAllLines(@"D:\personnes.csv");
+            foreach (var ligne in lignes)
+            {
+                var tab = ligne.Split(';');
+                personnes.Add(tab[0], tab[1]);
+            }
+
+            // Partie 1 : saisie
+            while (true)
             {
                 Console.Write("Nom : ");
                 var nom = Console.ReadLine();
                 if (string.IsNullOrEmpty(nom)) break;
                 Console.Write("Ville : ");
                 var ville = Console.ReadLine();
-                personnes.Add(nom, ville);
+                if (personnes.ContainsKey(nom))
+                    Console.WriteLine("J'ai déjà un {0}", nom);
+                else
+                    personnes.Add(nom, ville);
             }
+
+            // Partie 1.1 : sauvegarder
+            var contenu = "";
+            foreach (var person in personnes)
+            {
+                contenu += person.Key + ";" + person.Value + "\n";
+            }
+            File.WriteAllText(@"D:\personnes.csv", contenu);
+
 
             // Partie 2 : affichage
-            Console.ForegroundColor = ConsoleColor.Cyan;
-            foreach(var person in personnes)
-            {
-                Console.WriteLine("{0}\t{1}", person.Key, person.Value );
-            }
+            Affichage();
 
             // Partie : recherche de ville
-            Console.ForegroundColor = ConsoleColor.Gray;
-            Console.Write("Nom ?");
+            Console.Write("Nom ? ");
             var question = Console.ReadLine();
             if (personnes.ContainsKey(question))
             {
-                Console.WriteLine("{0} est de la ville de {1}", 
-                    question, 
+                Console.WriteLine("{0} est de la ville de {1}",
+                    question,
                     personnes[question]);
             }
             else
@@ -44,6 +60,16 @@ namespace Algo5
                 Console.WriteLine("{0} n'existe pas !", question);
             }
             Console.ReadLine();
+        }
+
+        private static void Affichage()
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            foreach (var person in personnes)
+            {
+                Console.WriteLine("{0}\t{1}", person.Key, person.Value);
+            }
+            Console.ForegroundColor = ConsoleColor.Gray;
         }
     }
 }
