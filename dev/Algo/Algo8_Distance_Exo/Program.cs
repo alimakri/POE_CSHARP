964f3dcfd7be57708d889dc1b020b3c2331a01dd
Ali MAKRI <github@makri.fr>
2023-03-21 17:02:20 +0100

    17:02

---
+++ dev/Algo/Algo8_Distance_Exo/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Algo8_Distance_Exo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Liste de ville 
+
+            // Liste de x
+
+            // Liste de y
+
+
+            // Distance la plus courte
+        }
+    }
+}
