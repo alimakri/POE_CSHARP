3827b59f0b557ad3d6770e68d66099842131e480
Ali MAKRI <github@makri.fr>
2023-03-21 12:29:59 +0100

    12:30

--- dev/Algo/Algo5/Program.cs
+++ dev/Algo/Algo5/Program.cs
@@ -10,6 +10,7 @@ namespace Algo5
     {
         static void Main(string[] args)
         {
+            // Partie 1 : saisie
             var personnes = new Dictionary<string, string>();
             while(true)
             {
@@ -20,10 +21,27 @@ namespace Algo5
                 var ville = Console.ReadLine();
                 personnes.Add(nom, ville);
             }
+
+            // Partie 2 : affichage
             Console.ForegroundColor = ConsoleColor.Cyan;
             foreach(var person in personnes)
             {
                 Console.WriteLine("{0}\t{1}", person.Key, person.Value );
+            }
+
+            // Partie : recherche de ville
+            Console.ForegroundColor = ConsoleColor.Gray;
+            Console.Write("Nom ?");
+            var question = Console.ReadLine();
+            if (personnes.ContainsKey(question))
+            {
+                Console.WriteLine("{0} est de la ville de {1}", 
+                    question, 
+                    personnes[question]);
+            }
+            else
+            {
+                Console.WriteLine("{0} n'existe pas !", question);
             }
             Console.ReadLine();
         }
