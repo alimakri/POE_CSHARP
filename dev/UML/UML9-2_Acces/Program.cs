694bde17df48d00061f821ea087dbc4c4a2a536c
Ali MAKRI <github@makri.fr>
2023-03-23 17:01:51 +0100

    17:01

--- dev/UML/UML9-2_Acces/Program.cs
+++ dev/UML/UML9-2_Acces/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using UML9_Acces;
 
 namespace UML9_2_Acces
 {
@@ -14,10 +15,21 @@ namespace UML9_2_Acces
     }
     public class D : A
     {
-
+        public void M()
+        {
+            var x = P1;
+            var x3 = P3;
+            var x5 = P5;
+            var x11 = P11;
+        }
     }
     public class E
     {
-
+        public void M()
+        {
+            var a1 = new A();
+            var x = a1.P1;
+            var x11 = A.P11;
+        }
     }
 }
--- dev/UML/UML9_Acces/Program.cs
+++ dev/UML/UML9_Acces/Program.cs
@@ -14,18 +14,40 @@ namespace UML9_Acces
     }
     public class A
     {
-        public string P1;
+        public string P1 = "Propriété P1";
         private string P2;
         protected string P3;
         internal string P4;
         internal protected string P5;
+        public static string P11;
+        public void M()
+        {
+            var x1 = P1;
+            var x2 = P2;
+            var x3 = P3;
+            var x4 = P4;
+            var x5 = P5;
+        }
     }
     public class B : A
     {
-
+        public void M()
+        {
+            var x1 = P1;
+            var x3 = P3;
+            var x4 = P4;
+            var x5 = P5;
+            var x11 = P11;
+        }
     }
     public class C
     {
-
+        public void M()
+        {
+            var a1 = new A();
+            var x = a1.P1;
+            var x4 = a1.P4;
+            var x11 = A.P11;
+        }
     }
 }
