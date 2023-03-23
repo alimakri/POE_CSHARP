18acd060684e2596e56a79c720b0e5b021277f67
Ali MAKRI <github@makri.fr>
2023-03-23 16:42:28 +0100

    16:42

---
+++ dev/UML/UML9-2_Acces/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UML9_2_Acces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class D : A
+    {
+
+    }
+    public class E
+    {
+
+    }
+}
---
+++ dev/UML/UML9_Acces/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UML9_Acces
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    public class A
+    {
+        public string P1;
+        private string P2;
+        protected string P3;
+        internal string P4;
+        internal protected string P5;
+    }
+    public class B : A
+    {
+
+    }
+    public class C
+    {
+
+    }
+}
