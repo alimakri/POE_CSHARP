3a6d7575da9007a4f7b4d5e83423304bb528e682
Ali MAKRI <github@makri.fr>
2023-03-24 12:23:37 +0100

    12:23

--- dev/UML/UMLTD4/Program.cs
+++ dev/UML/UMLTD4/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,27 +11,75 @@ namespace UMLTD4
     {
         static void Main(string[] args)
         {
-           
+
             var liste = new Personnes();
             liste.Add(new Personne { Nom = "Albert" });
             liste.Add(new Personne { Nom = "Xavier" });
-            for (int i = 0; i < liste.Count; i++)
+            //for (int i = 0; i < liste.Count; i++)
+            //{
+            //    Console.WriteLine(liste[i].Nom);
+            //}
+            foreach (var p in liste)
             {
-                Console.WriteLine(liste.DonneMoi(i).Nom);
+                Console.WriteLine(p.Nom);
             }
+            Console.ReadLine(); 
         }
     }
     class Personne
     {
         public string Nom;
     }
-    class Personnes
+    class PersonneEnumerator : IEnumerator<Personne>
+    {
+        private List<Personne> Liste;
+        public PersonneEnumerator(List<Personne> liste)
+        {
+            Liste = liste;
+        }
+        private int Index = -1;
+        public Personne Current
+        {
+            get { return Liste[Index]; }
+        }
+
+        object IEnumerator.Current
+        {
+            get { return Liste[Index]; }
+        }
+
+        public void Dispose() { }
+
+        public bool MoveNext()
+        {
+            Index++;
+            return Index < Liste.Count;
+        }
+
+        public void Reset()
+        {
+            Index = -1;
+        }
+    }
+    class Personnes : IEnumerable<Personne>
     {
         private List<Personne> ListeInterne = new List<Personne>();
         public void Add(Personne p)
         {
             ListeInterne.Add(p);
         }
+
+        public IEnumerator<Personne> GetEnumerator()
+        {
+            return new PersonneEnumerator(ListeInterne);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return new PersonneEnumerator(ListeInterne);
+        }
+
+
         //public int Count()
         //{
         //    return ListeInterne.Count;
@@ -39,9 +88,13 @@ namespace UMLTD4
         {
             get { return ListeInterne.Count; }
         }
-        public Personne DonneMoi(int index)
+        // Indexeur
+        public Personne this[int index]
         {
-            return ListeInterne[index];
+            get
+            {
+                return ListeInterne[index];
+            }
         }
     }
 }
