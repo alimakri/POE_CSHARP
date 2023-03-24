8c5c7977a5ec62e264b0c91b3071b47f76922274
Ali MAKRI <github@makri.fr>
2023-03-24 11:40:25 +0100

    11:40

---
+++ dev/UML/UMLTD4/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UMLTD4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           
+            var liste = new Personnes();
+            liste.Add(new Personne { Nom = "Albert" });
+            liste.Add(new Personne { Nom = "Xavier" });
+            for (int i = 0; i < liste.Count; i++)
+            {
+                Console.WriteLine(liste.DonneMoi(i).Nom);
+            }
+        }
+    }
+    class Personne
+    {
+        public string Nom;
+    }
+    class Personnes
+    {
+        private List<Personne> ListeInterne = new List<Personne>();
+        public void Add(Personne p)
+        {
+            ListeInterne.Add(p);
+        }
+        //public int Count()
+        //{
+        //    return ListeInterne.Count;
+        //}
+        public int Count
+        {
+            get { return ListeInterne.Count; }
+        }
+        public Personne DonneMoi(int index)
+        {
+            return ListeInterne[index];
+        }
+    }
+}
