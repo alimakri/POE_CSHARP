843d233a8f246063f8439c6a87278b10c17af6e3
Ali MAKRI <github@makri.fr>
2023-03-24 11:02:57 +0100

    11:02

---
+++ dev/UML/UMLTD3/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UMLTD3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var vache = new Bovide();
+            Console.WriteLine(vache);
+            vache.Traire();
+            Console.ReadLine();
+        }
+    }
+    class Animal
+    {
+        public override string ToString()
+        {
+            return "Animal";
+        }
+    }
+    class Mamiffere : Animal
+    {
+        public override string ToString()
+        {
+            var x = base.ToString();
+            return x + "- Mamiffere";
+        }
+        public virtual void Traire()
+        {
+            Console.WriteLine("Traite de Mamiffere");
+        }
+    }
+    class Bovide :Mamiffere
+    {
+        public override string ToString()
+        {
+            var x = base.ToString();
+            return x + "- Bovide";
+        }
+        public override void Traire()
+        {
+            Console.WriteLine("Traite de Bovide");
+        }
+    }
+}
