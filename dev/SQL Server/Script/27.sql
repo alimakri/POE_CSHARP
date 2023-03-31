cda8b2e834c98406e3db13dd0f6ba66f2bbdd0e8
Ali MAKRI <github@makri.fr>
2023-03-31 16:59:37 +0200

    16:59

--- dev/SQL Server/Script/27.sql
+++ dev/SQL Server/Script/27.sql
@@ -53,3 +53,49 @@ SELECT  h.SalesOrderNumber
  GROUP BY c.ProductCategoryID
  ORDER BY SUM(OrderQty * UnitPrice) DESC
  )
+ -- Version Temp
+  SELECT TOP 1 c.ProductCategoryID categorie, SUM(OrderQty * UnitPrice) CA
+  INTO #tab_tempo
+  FROM Sales.SalesOrderDetail d
+  inner join PRODUCTION.Product p on p.ProductID = d.ProductID
+  INNER JOIN Production.ProductSubcategory s ON s.ProductSubcategoryID = p.ProductSubcategoryID
+  INNER JOIN Production.ProductCategory c ON c.ProductCategoryID = s.ProductCategoryID
+  GROUP BY c.ProductCategoryID  
+  ORDER BY SUM(OrderQty * UnitPrice) DESC
+
+ 
+
+  SELECT  h.SalesOrderNumber
+ FROM Sales.SalesOrderDetail d
+ INNER JOIN Sales.SalesOrderHeader h ON h.SalesOrderID = d.SalesOrderID
+ INNER JOIN PRODUCTION.Product p on p.ProductID = d.ProductID
+ INNER JOIN Production.ProductSubcategory s ON s.ProductSubcategoryID = p.ProductSubcategoryID
+ INNER JOIN Production.ProductCategory c ON c.ProductCategoryID = s.ProductCategoryID
+ WHERE c.ProductCategoryID IN 
+ (
+  SELECT categorie
+  FROM #tab_tempo
+ )
+-- Version CTE
+ WITH t1 (categorie,CA)
+as
+(
+Select top 1 pc.ProductCategoryID categorie , SUM (OrderQty * UnitPrice) CA
+FROM Sales.SalesOrderDetail sod
+INNER JOIN Production.Product p on p.ProductID = sod.ProductID
+INNER JOIN Production.ProductSubcategory ps on ps.ProductSubcategoryID = p.ProductSubcategoryID
+INNER JOIN Production.ProductCategory pc on pc.ProductCategoryID = ps.ProductCategoryID
+GROUP BY pc.ProductCategoryID
+ORDER BY SUM (OrderQty * UnitPrice)  desc
+)
+select h.SalesOrderNumber
+from Sales.SalesOrderDetail sod
+INNER JOIN Sales.SalesOrderHeader h on h.SalesOrderID  = sod.SalesOrderID
+INNER JOIN Production.Product p on p.ProductID = sod.ProductID
+Inner join Production.ProductSubcategory ps on ps.ProductSubcategoryID = p.ProductSubcategoryID
+inner join Production.ProductCategory pc on pc.ProductCategoryID = ps.ProductCategoryID
+WHERE pc.ProductCategoryID in
+(
+Select categorie
+FROM t1
+)
