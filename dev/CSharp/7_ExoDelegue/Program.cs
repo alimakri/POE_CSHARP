620fb4b5bc683a878b23d0194db6fa7d950a3d9a
Ali MAKRI <github@makri.fr>
2023-04-03 15:06:04 +0200

    15:05

--- dev/CSharp/7_ExoDelegue/Program.cs
+++ dev/CSharp/7_ExoDelegue/Program.cs
@@ -11,9 +11,9 @@ namespace _7_ExoDelegue
     {
         static void Main(string[] args)
         {
-            var c = new Compteur { Depart = 1, Arrivee = 10, Couleur = ConsoleColor.Green, Pause= 1000 };
-
-            Thread.Sleep(1000);
+            var c = new Compteur { Depart = 1, Arrivee = 10, Couleur = ConsoleColor.Green, Pause = 1000 };
+            c.Afficher();
+            Console.ReadLine();
         }
     }
     class Compteur
@@ -22,5 +22,15 @@ namespace _7_ExoDelegue
         public int Arrivee;
         public ConsoleColor Couleur;
         public int Pause;
+
+        internal void Afficher()
+        {
+            for (int i = Depart; i <= Arrivee; i++)
+            {
+                Console.ForegroundColor = Couleur;
+                Console.WriteLine(i);
+                Thread.Sleep(1000);
+            }
+        }
     }
 }
