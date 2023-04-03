bb5a80c6c1febc76bd2d3e202888c841c712fcae
Ali MAKRI <github@makri.fr>
2023-04-03 16:55:53 +0200

    16:55

--- dev/CSharp/9_Linq_Exo/Program.cs
+++ dev/CSharp/9_Linq_Exo/Program.cs
@@ -10,6 +10,23 @@ namespace _9_Linq_Exo
     {
         static void Main(string[] args)
         {
+            List<Pays> lesPays = new List<Pays>
+            {
+                new Pays{Nom="Italie", Capital="Rome" },
+                new Pays{Nom="Vatican", Capital="Rome" },
+                new Pays{Nom="Chili", Capital= "Santiago"},
+                new Pays{Nom="Chili", Capital= "Valparaiso"},
+                new Pays{Nom="Israel", Capital= "Jerusalem"},
+                new Pays{Nom="Palestine", Capital= "Jerusalem"}
+            };
+            // Tous les pays capital Rome
+            var romes = lesPays.Where(pays => pays.Capital == "Rome");
+
+            // Capitales du chili
+            var chili = lesPays.Where(x => x.Nom == "Chili").ToArray();
+
+            // Pays de Jerusalem
+            var qods = lesPays.Where(pays => pays.Capital == "Jerusalem");
         }
     }
     class Pays
