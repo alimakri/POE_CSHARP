66589b06bfbd85d5b758e30b39ad38a09fda1e76
Ali MAKRI <github@makri.fr>
2023-04-03 10:41:38 +0200

    10:41

---
+++ dev/CSharp/Portee/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Portee
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class C1
+    {
+        public int P1;
+        public void M1(int d)
+        {
+            bool b = true;
+            while (b) ;
+            int P3 = 0;
+            var x = P1;
+            var y = P3;
+            {
+                int P4 = 0;
+                int z = P4;
+                int k = h;
+                int h = P1;
+                int f = d;
+                ;
+            }
+            var w = P4;
+        }
+        public void M2()
+        {
+            var y = P3;
+        }
+    }
+    class C2
+    {
+        public int P2;
+        public void M2()
+        {
+            var x = P1;
+        }
+    }
+}
