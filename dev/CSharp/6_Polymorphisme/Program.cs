f449219360a645a6978c4bdfeda2c1db16c479f8
Ali MAKRI <github@makri.fr>
2023-04-03 13:56:59 +0200

    13:56

---
+++ dev/CSharp/6_Polymorphisme/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _6_Polymorphisme
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Vehicule v;
+            if (Console.ReadLine() == "A")
+            {
+                v = new Auto();
+            }
+            else
+            {
+                v = new Moto();
+            }
+            v.Rouler();
+        }
+    }
+    class Vehicule
+    {
+        public virtual void Rouler()
+        {
+            Console.WriteLine("Le véhicule roule");
+        }
+    }
+    class Auto : Vehicule
+    {
+        public override void Rouler()
+        {
+            Console.WriteLine("L'auto roule");
+        }
+    }
+    class Moto : Vehicule
+    {
+        public override void Rouler()
+        {
+            Console.WriteLine("La moto roule");
+        }
+    }
+    class Kawasaki : Moto
+    {
+        public override void Rouler()
+        {
+            Console.WriteLine("La moto roule");
+        }
+    }
+}
---
+++ dev/CSharp/7_Delegue/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _7_Delegue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+    class Centrale
+    {
+
+    }
+    class PompeHydraulique
+    {
+        public void Refroidir() { Console.WriteLine("La pome hydraulique est lancée !"); }
+    }
+    class PompeElectrique
+    {
+        public void Refroidir() { Console.WriteLine("La pome electrique est lancée !"); }
+    }
+}
