96b09abb4d95dcae10c077a5d376a080672cf336
Ali MAKRI <github@makri.fr>
2023-04-03 14:20:50 +0200

    14:20 Delegue Version 1

--- dev/CSharp/7_Delegue/Program.cs
+++ dev/CSharp/7_Delegue/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,22 +7,57 @@ using System.Threading.Tasks;
 
 namespace _7_Delegue
 {
+    //  Version 1
     class Program
     {
         static void Main(string[] args)
         {
+            var c = new Centrale();
+            c.RefroidirTout();
+            Console.ReadLine();
         }
     }
     class Centrale
     {
-
+        public ArrayList Pompes;
+        public Centrale()
+        {
+            Pompes = new ArrayList();
+            var p1 = new PompeHydraulique();
+            var p2 = new PompeHydraulique();
+            var p3 = new PompeElectrique();
+            var p4 = new PompeManuelle();
+            Pompes.Add(p1);
+            Pompes.Add(p2);
+            Pompes.Add(p3);
+            Pompes.Add(p4);
+        }
+        public void RefroidirTout()
+        {
+            foreach (object p in Pompes)
+            {
+                if (p is PompeHydraulique)
+                {
+                    ((PompeHydraulique)p).Refroidir();
+                }
+                else if (p is PompeElectrique)
+                {
+                    ((PompeElectrique)p).Refroidir();
+                }
+                else throw new Exception("Grave Erreur");
+            }
+        }
     }
     class PompeHydraulique
     {
-        public void Refroidir() { Console.WriteLine("La pome hydraulique est lancée !"); }
+        public void Refroidir() { Console.WriteLine("La pompe hydraulique est lancée !"); }
     }
     class PompeElectrique
     {
-        public void Refroidir() { Console.WriteLine("La pome electrique est lancée !"); }
+        public void Refroidir() { Console.WriteLine("La pompe electrique est lancée !"); }
+    }
+    class PompeManuelle
+    {
+        public void Refroidir() { Console.WriteLine("La pompe manuelle est lancée !"); }
     }
 }
