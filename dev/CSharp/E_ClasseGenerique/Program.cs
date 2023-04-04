cbd148a55d6abf568b2959d6a0d6e51dbf917cf3
Ali MAKRI <github@makri.fr>
2023-04-04 14:08:45 +0200

    14:08

---
+++ dev/CSharp/E_ClasseGenerique/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace E_ClasseGenerique
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var a1 = new ActiviteSportive<Foot, TerrainFoot>();
+            var a2 = new ActiviteSportive<Rugby, TerrainRugby>();
+            //var a3 = new ActiviteSportive<Natation>();
+        }
+    }
+    class ActiviteSportive<T, U> 
+        where T : SportAvecBalle, new()
+        where U : Terrain, new()
+    {
+        public ActiviteSportive()
+        {
+            T f = new T();
+            U terrain = new U();
+        }
+    }
+
+    //class ActiviteSportiveFoot
+    //{
+    //    public ActiviteSportiveFoot()
+    //    {
+    //        Foot f = new Foot();
+    //    }
+    //}
+    //class ActiviteSportiveRugby
+    //{
+    //    public ActiviteSportiveRugby()
+    //    {
+    //        Rugby f = new Rugby();
+    //    }
+    //}
+    class Foot : SportAvecBalle { }
+    class Rugby : SportAvecBalle { }
+    class SportAvecBalle
+    {
+
+    }
+    class Natation { }
+    class TerrainFoot : Terrain { }
+    class TerrainRugby : Terrain { }
+    class Terrain { }
+    class Piscine { }
+}
