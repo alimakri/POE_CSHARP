565f29f51b614726b280cfd7b391306119d0f6be
Ali MAKRI <github@makri.fr>
2023-04-04 16:08:17 +0200

    16:08

--- dev/CSharp/G_LambdaExpression/Program.cs
+++ dev/CSharp/G_LambdaExpression/Program.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace G_LambdaExpression
 {
-    delegate bool PariteDelegue(int n);
+    //delegate bool PariteDelegue(int n);
     class Program
     {
         static void Main(string[] args)
@@ -14,18 +14,20 @@ namespace G_LambdaExpression
             List<int> resultat = new List<int>();
             var nombres = new List<int> { 2, 8, 9, 6, 7, 12, 17, 25, 39, 1 };
 
-            Compute(nombres, resultat, new PariteDelegue(isImpaire));
+            //Compute(nombres, resultat, new PariteDelegue(isImpaire));
+            var nombresImpaires = nombres.Where(x => x % 2 == 1);
+
         }
 
-        private static void Compute(List<int> nombres, List<int> resultat, PariteDelegue f)
-        {
-            foreach (var nombre in nombres)
-            {
-                if (f(nombre)) resultat.Add(nombre);
-            }
-        }
+        //private static void Compute(List<int> nombres, List<int> resultat, PariteDelegue f)
+        //{
+        //    foreach (var nombre in nombres)
+        //    {
+        //        if (f(nombre)) resultat.Add(nombre);
+        //    }
+        //}
 
-        static bool isPaire(int n) { return n % 2 == 0; }
-        static bool isImpaire(int n) { return n % 2 == 1; }
+        //static bool isPaire(int n) { return n % 2 == 0; }
+        //static bool isImpaire(int n) { return n % 2 == 1; }
     }
 }
