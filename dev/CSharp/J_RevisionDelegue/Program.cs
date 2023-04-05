6b6e80ddb88072eb55a06968425ea58cac643d27
Ali MAKRI <github@makri.fr>
2023-04-05 10:42:29 +0200

    10:42

---
+++ dev/CSharp/J_RevisionDelegue/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace J_RevisionDelegue
+{
+    delegate int Operation(int a, int b);
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new Operation(Addition);
+            var c = new Calculatrice(d);
+            var resultat = c.Calculer(3, 7);
+            
+        }
+        static int Addition(int a, int b) { return a + b; }
+        static int Multiplication(int a, int b) { return a * b; }
+    }
+    class Calculatrice
+    {
+        private Operation Op = null;
+        public Calculatrice(Operation delegue)
+        {
+            Op = delegue;
+        }
+        public int Calculer(int op1, int op2)
+        {
+            return Op.Invoke(op1, op2);
+        }
+    }
+}
---
+++ dev/CSharp/J_RevisionDelegueV2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace J_RevisionDelegue
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var c = new Calculatrice((x, y) => x + y);
+            //var c = new Calculatrice((x, y) =>
+            //{
+            //    double total = 1;
+            //    for (int i = 1; i <= y; i++)
+            //    {
+            //        total *= x;
+            //    }
+            //    return total;
+            //});
+            var resultat = c.Calculer(3, 7);
+        }
+    }
+    class Calculatrice
+    {
+        private Func<double, double, double> Op = null;
+        public Calculatrice(Func<double, double, double> delegue)
+        {
+            Op = delegue;
+        }
+        public double Calculer(double op1, double op2)
+        {
+            return Op(op1, op2);
+        }
+    }
+}
