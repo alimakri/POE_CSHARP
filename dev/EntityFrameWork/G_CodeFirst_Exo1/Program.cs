ef088631fdb60946d95a86387d5fbc534e88643d
Ali MAKRI <github@makri.fr>
2023-04-13 10:04:07 +0200

    10:03

--- dev/EntityFrameWork/G_CodeFirst_Exo1/Program.cs
+++ dev/EntityFrameWork/G_CodeFirst_Exo1/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Data.Entity;
 using System.Linq;
 using System.Text;
@@ -12,6 +13,15 @@ namespace G_CodeFirst_Exo1
     {
         static void Main(string[] args)
         {
+            var context = new OccupationContext();
+            var p1 = new Piscine { Nom = "Piscine 1", Capacite = 250 };
+            var p2 = new Piscine { Nom = "Piscine 2", Capacite = 410 };
+            var a1 = new Acces { Type = "Bus 45", LesPiscines = new List<Piscine> { p1, p2} };
+            p1.LAcces = a1;
+            context.LesPiscines.Add(p1);
+            context.LesPiscines.Add(p2);
+            context.LesAcces.Add(a1);
+            context.SaveChanges();
         }
     }
     public class Piscine
@@ -20,7 +30,8 @@ namespace G_CodeFirst_Exo1
         [Required]
         public string Nom { get; set; }
         public int Capacite { get; set; }
-        public Acces UnAcces { get; set; }
+        public Acces LAcces { get; set; }
+        public List<Activite> LesActivites { get; set; }
     }
     public class Acces
     {
@@ -28,14 +39,16 @@ namespace G_CodeFirst_Exo1
         [Required]
         public string Type { get; set; }
         [Required]
-        public Piscine UnePiscine { get; set; }
+        public List<Piscine> LesPiscines { get; set; }
     }
     public class Activite
     {
         public int Id { get; set; }
         [Required]
+        [Column(TypeName ="DateTime2")]
         public DateTime DateDebut { get; set; }
         [Required]
+        [Column(TypeName = "DateTime2")]
         public DateTime DateFin { get; set; }
         [Required]
         public string Type { get; set; }
