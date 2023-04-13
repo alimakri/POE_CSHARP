1454d06d52ea37ad72a3a7f4bafe8b82912dde40
Ali MAKRI <github@makri.fr>
2023-04-13 11:43:51 +0200

    11:43

--- dev/EntityFrameWork/H_CodeFirst_Core/Entites.cs
+++ dev/EntityFrameWork/H_CodeFirst_Core/Entites.cs
@@ -10,6 +10,8 @@ namespace H_CodeFirst_Core
     // Install-Package Microsoft.EntityFrameworkCore.SqlServer
     // Install-Package Microsoft.EntityFrameworkCore.Tools -Version 7.0.5
     // Add-Migration CreateSchoolDB
+    // update-database –verbose
+    // https://1drv.ms/u/s!AmBkQr-1-ilzhIg2x-RbosXDajMP7A?e=36wS2V
     public class Student
     {
         public int StudentId { get; set; }
@@ -26,7 +28,7 @@ namespace H_CodeFirst_Core
         public DbSet<Course> Courses { get; set; }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseSqlServer(@"data source=.\SQLEXPRESS;initial catalog=SchoolDB;integrated security=True;");
+            optionsBuilder.UseSqlServer(@"data source=.\SQLEXPRESS;initial catalog=SchoolDB;integrated security=True;trustServerCertificate=true");
         }
     }
 }
