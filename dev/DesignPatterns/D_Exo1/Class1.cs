8a766fa8798f4aed43d347209fd456d1d33048e7
Ali MAKRI <github@makri.fr>
2023-04-14 13:56:57 +0200

    13:56

---
+++ dev/DesignPatterns/D_Exo1/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace D_Exo1
+{
+    public class Directeur
+    {
+        public void GetSalaire()
+        {
+            return 2000;
+        }
+    }
+    public class Employe
+    {
+        public void GetSalaire()
+        {
+            return 2000;
+        }
+    }
+}
