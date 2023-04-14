5bbc23b8c5580aa943bb6adcf2f6a2683982dd2a
Ali MAKRI <github@makri.fr>
2023-04-14 10:33:28 +0200

    10:33

---
+++ dev/DesignPatterns/A_Singleton/MemoryCache.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_Singleton
+{
+    public class MemoryCache
+    {
+        private Dictionary<string, object> CacheObjects;
+        private static MemoryCache Cache = new MemoryCache();
+        public static MemoryCache Instance => Cache;
+        //{
+        //    get
+        //    {
+        //        return Cache;
+        //    }
+        //}
+        private MemoryCache()
+        {
+            CacheObjects = new ();
+        }
+        public void Add(string key, object valeur)
+        {
+            CacheObjects.Add(key, valeur);
+        }
+        public object? Get(string key)
+        {
+            if (CacheObjects.ContainsKey(key)) return CacheObjects[key];
+            return null;
+        }
+    }
+}
---
+++ dev/DesignPatterns/A_Singleton/Program.cs
@@ -0,0 +1,12 @@
+﻿using A_Singleton;
+
+var cache1 = MemoryCache.Instance;
+var cache2 = MemoryCache.Instance;
+
+cache1.Add("test", DateTime.Now);
+var d = cache2.Get("test");
+
+Console.WriteLine(d);
+Console.WriteLine(Object.ReferenceEquals(cache2, cache1));
+
+
