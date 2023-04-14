d42c388261316aa734454b6548bf4e24fff357b6
Ali MAKRI <github@makri.fr>
2023-04-14 15:02:07 +0200

    15:02

--- dev/DesignPatterns/D_Exo1/Class1.cs
+++ dev/DesignPatterns/D_Exo1/Class1.cs
@@ -6,37 +6,84 @@ using System.Threading.Tasks;
 
 namespace D_Exo1
 {
-    public interface ISalarie
+    #region Avec interface
+    //public interface ISalarie
+    //{
+    //    decimal GetSalaire();
+    //    decimal Prime { get; }
+    //}
+    //public class Directeur : ISalarie
+    //{
+    //    public Directeur(decimal prime)
+    //    {
+    //        Prime = prime;
+    //    }
+
+    //    public decimal Prime { get; set; }
+
+    //    public decimal GetSalaire() => 5000 * Prime;
+
+    //}
+    //public class Employe : ISalarie
+    //{
+    //    public Employe(decimal prime)
+    //    {
+    //        Prime = prime;
+    //    }
+    //    public decimal Prime { get; set; }
+    //    public decimal GetSalaire() => 2000 * Prime;
+    //}
+    ////  ChefdeProjet
+    //public class ChefdeProjet : ISalarie
+    //{
+    //    public decimal Prime { get; set; }
+
+    //    public decimal GetSalaire() => 3000 * Prime;
+    //}
+    #endregion
+
+    #region Suite 
+    public abstract class Salarie
     {
-        decimal GetSalaire();
-        decimal Prime { get; }
+        public abstract decimal GetSalaire();
+        public virtual decimal Prime { get; set; }
+        protected Salarie(decimal prime)
+        {
+
+        }
     }
-    public class Directeur : ISalarie
+    public class Directeur : Salarie
     {
-        public Directeur(decimal prime)
+        public Directeur(decimal prime) : base(prime)
         {
             Prime = prime;
         }
 
-        public decimal Prime { get; set; }
+        public override decimal Prime { get; set; }
 
-        public decimal GetSalaire() => 5000 * Prime;
+        public override decimal GetSalaire() => 5000 * Prime;
 
     }
-    public class Employe : ISalarie
+    public class Employe : Salarie
     {
-        public Employe(decimal prime)
+        public Employe(decimal prime) : base(prime)
         {
             Prime = prime;
         }
-        public decimal Prime { get; set; }
-        public decimal GetSalaire() => 2000 * Prime;
+        public override decimal Prime { get; set; }
+        public override decimal GetSalaire() => 2000 * Prime;
     }
     //  ChefdeProjet
-    public class ChefdeProjet : ISalarie
+    public class ChefdeProjet : Salarie
     {
-        public decimal Prime { get; set; }
+        public ChefdeProjet(decimal prime) : base(prime)
+        {
+            Prime = prime;
+        }
 
-        public decimal GetSalaire() => 3000 * Prime;
+        public override decimal Prime { get; set; }
+
+        public override decimal GetSalaire() => 3000 * Prime;
     }
+    #endregion
 }
--- dev/DesignPatterns/D_Exo1/Program.cs
+++ dev/DesignPatterns/D_Exo1/Program.cs
@@ -2,7 +2,7 @@
 
 Directeur albert = new Directeur(1.2M);
 var alfred = new Employe(1);
-ISalarie aymeric = new ChefdeProjet();
+ChefdeProjet aymeric = new ChefdeProjet();
 
 var salaire1 = albert.GetSalaire();
 var salaire2 = alfred.GetSalaire();
