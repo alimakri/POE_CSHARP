f08c46c71de14644d4275f0d8c9b50017817502d
Ali MAKRI <github@makri.fr>
2023-04-17 17:00:07 +0200

    17:00

---
+++ dev/DesignPatterns/O_Exo5/Class1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace O_Exo5
+{
+    public class Personne
+    {
+        public Personne? Parent1 = null;
+        public Personne? Parent2 = null;
+    }
+    public static class Genealogie
+    {
+        public static bool SontFreres(Personne p1, Personne p2)
+        {
+            return (p1.Parent1 == p2.Parent1 && p1.Parent2 == p2.Parent2);
+        }
+        public static bool SontDemiFreres(Personne p1, Personne p2)
+        {
+            return ((p1.Parent1 == p2.Parent2 && p1.Parent1 != null) || (p1.Parent2 == p2.Parent1 && p1.Parent2 != null));
+        }
+        public static bool SontCousins(Personne p1, Personne p2)
+        {
+            if (SontFreres(p1, p2) || SontDemiFreres(p1,p2)) return false;
+            var listeP1 = new List<Personne?> { p1.Parent1?.Parent1, p1.Parent1?.Parent2, p1.Parent2?.Parent1, p1.Parent2?.Parent2 }
+                                .Where(gp=>gp != null);
+            var listeP2 = new List<Personne?> { p2.Parent1?.Parent1, p2.Parent1?.Parent2, p2.Parent2?.Parent1, p2.Parent2?.Parent2 }
+                                .Where(gp => gp != null);
+            var listeEnCommun = listeP1.Intersect(listeP2);
+            return listeEnCommun.Count() > 0;
+        }
+    }
+}
---
+++ dev/DesignPatterns/O_Exo5/Program.cs
@@ -0,0 +1,19 @@
+﻿using O_Exo5;
+
+var becassine = new Personne();
+var becassineP2 = new Personne();
+var becassineP2P2 = new Personne();
+var alain = new Personne();
+
+var josephine = new Personne();
+var josephineP1 = new Personne();
+
+// ------------------------------------------------------
+becassine.Parent2 = becassineP2;
+becassineP2.Parent2 = becassineP2P2;
+josephine.Parent1 = josephineP1;
+josephineP1.Parent1 = becassineP2P2;
+alain.Parent1 = josephineP1;
+// ------------------------------------------------------
+
+Console.WriteLine( Genealogie.SontCousins(becassine, josephine));
