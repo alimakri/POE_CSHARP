83fbb77be3322c803843ee5d9c019cdca6e714b5
Ali MAKRI <github@makri.fr>
2023-04-17 11:52:54 +0200

    11:52

---
+++ dev/DesignPatterns/I_Adapter/Personne.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace I_Adapter
+{
+    public interface IPersonneAdapter
+    {
+        Personne GetPersonne();
+    }
+
+
+    public class PersonneAdapter : IPersonneAdapter
+    {
+        private ExternalPersonneApiService Service;
+
+        public PersonneAdapter(ExternalPersonneApiService service)
+        {
+            Service = service;
+        }
+
+        public Personne GetPersonne()
+        {
+            var personneExterne = Service.GetPersonne();
+            var noms = personneExterne.FullName.Split(' ');
+            return new Personne
+            {
+                Id = personneExterne.Id,
+                LastName = noms[0],
+                FirstName = noms[1]
+            };
+        }
+    }
+    #region Classes
+    public class ExternalPersonneApiService
+    {
+        public PersonneExternal GetPersonne() => new() { Id = 1, FullName = "MAKRI Ali" };
+
+    }
+    public class Personne
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+    }
+    public class PersonneExternal
+    {
+        public int Id { get; set; }
+        public string FullName { get; set; }
+    }
+    #endregion
+}
---
+++ dev/DesignPatterns/I_Adapter/Program.cs
@@ -0,0 +1,7 @@
+﻿using I_Adapter;
+
+var adapter = new PersonneAdapter(new ExternalPersonneApiService());
+
+var p = adapter.GetPersonne();
+Console.WriteLine(p.LastName);
+Console.WriteLine(p.FirstName);
