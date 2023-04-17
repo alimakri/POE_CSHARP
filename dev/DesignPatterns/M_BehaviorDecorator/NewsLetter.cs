c9c685bc48e6e596d20f68e2c4e826c0b35f2fd4
Ali MAKRI <github@makri.fr>
2023-04-17 15:04:00 +0200

    15:03

---
+++ dev/DesignPatterns/M_BehaviorDecorator/NewsLetter.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M_BehaviorDecorator
+{
+    public interface INewsLetterSender
+    {
+        bool SendNewsLetter(string content);
+    }
+    public class AzureNewsLetterSender : INewsLetterSender
+    {
+        public bool SendNewsLetter(string content)
+        {
+            Console.WriteLine("Mails envoyés avec succès depuis Azure" + content);
+            return true;
+        }
+    }
+    public class NewsLetterSenderDecorator : INewsLetterSender
+    {
+        private readonly INewsLetterSender Sender;
+        public NewsLetterSenderDecorator(INewsLetterSender sender)
+        {
+            Sender = sender;
+        }
+        public virtual bool SendNewsLetter(string content)
+        {
+            return Sender.SendNewsLetter(content);
+        }
+    }
+    public class NewsLetterSenderDatabaseDecorator : NewsLetterSenderDecorator
+    {
+        public NewsLetterSenderDatabaseDecorator(INewsLetterSender sender) : base(sender)
+        {
+        }
+        public override bool SendNewsLetter(string content)
+        {
+            Console.WriteLine("Sauvegarde en BDD du contenu : {0}", content);
+            return base.SendNewsLetter(content);
+        }
+    }
+    public class NewsLetterSenderStatsDecorator : NewsLetterSenderDecorator
+    {
+        public static int NbNewsLetterSent = 0;
+        public NewsLetterSenderStatsDecorator(INewsLetterSender sender) : base(sender)
+        {
+        }
+        public override bool SendNewsLetter(string content)
+        {
+            NbNewsLetterSent++;
+            return base.SendNewsLetter(content);
+        }
+    }
+}
---
+++ dev/DesignPatterns/M_BehaviorDecorator/Program.cs
@@ -0,0 +1,10 @@
+﻿using M_BehaviorDecorator;
+
+var azure = new AzureNewsLetterSender();
+var db = new NewsLetterSenderDatabaseDecorator(azure);
+var stats = new NewsLetterSenderStatsDecorator(db);
+
+stats.SendNewsLetter("News Letter 1");
+stats.SendNewsLetter("News Letter 2");
+
+Console.WriteLine("Nb news letter envoyé: {0}", NewsLetterSenderStatsDecorator.NbNewsLetterSent);
