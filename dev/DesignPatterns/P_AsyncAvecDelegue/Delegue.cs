6c648b092552f580bbcfd0de6a21acf50a0b5f8f
Ali MAKRI <github@makri.fr>
2023-04-18 09:29:00 +0200

    09:28

---
+++ dev/DesignPatterns/P_AsyncAvecDelegue/Delegue.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace P_AsyncAvecDelegue
+{
+    public class Tache
+    {
+        public void Execution(string message, int temps)
+        {
+            Thread.Sleep(1000 * temps);
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.WriteLine("Exécution de la tâche {0}", message);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+        }
+    }
+    delegate void TacheDelegate(string a, int b);
+}
---
+++ dev/DesignPatterns/P_AsyncAvecDelegue/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P_AsyncAvecDelegue
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            var tache1 = new Tache();
+            var tache2 = new Tache();
+
+            Console.WriteLine(DateTime.Now);
+
+            // Version 1
+            //tache1.Execution("Tâche 1", 5);
+            //tache2.Execution("Tâche 2", 3);
+
+            // Version 2
+            //var dTache1 = new TacheDelegate(tache1.Execution);
+            //var dTache2 = new TacheDelegate(tache2.Execution);
+            //dTache1.Invoke("Tâche 1", 5);
+            //dTache1.Invoke("Tâche 2", 3);
+
+            // Version 3
+            var dTache1 = new TacheDelegate(tache1.Execution);
+            var dTache2 = new TacheDelegate(tache2.Execution);
+            dTache1.BeginInvoke("Tâche 1", 5, null, null);
+            dTache1.Invoke("Tâche 2", 3);
+
+
+            Console.WriteLine(DateTime.Now);
+
+            Console.ReadLine();
+        }
+    }
+}
