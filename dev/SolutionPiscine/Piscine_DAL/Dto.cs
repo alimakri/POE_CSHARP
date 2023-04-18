61bd787a5e93468ba6d27c98a1d09b358384c65b
Ali MAKRI <github@makri.fr>
2023-04-18 15:27:24 +0200

    15:27

---
+++ dev/SolutionPiscine/Piscine_DAL/Dto.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Piscine_DAL
+{
+    public static class DataTransferObject
+    {
+        public static List<Piscine> ToListPiscine(this ArrayList al)
+        {
+            List<Piscine> resultat = new List<Piscine>();
+            for (int i = 0; i < al.Count; i += 3)
+            {
+                resultat.Add(new Piscine
+                {
+                    Id = (int)al[i],
+                    Nom = (string)al[i + 1],
+                    Capacite = (int)al[i + 2]
+                });
+            }
+            return resultat;
+        }
+        public static List<Acces> ToListAcces(this ArrayList al)
+        {
+            List<Acces> resultat = new List<Acces>();
+            var toutesLesPiscines = Repository.GetAllPiscines();
+            for (int i = 0; i < al.Count; i += 3)
+            {
+                List<Piscine> piscines = new List<Piscine>();
+                foreach (var j in (int[])al[i + 2])
+                {
+                    var p = toutesLesPiscines.FirstOrDefault(x => x.Id == j);
+                    if (p != null) piscines.Add(p);
+                }
+                resultat.Add(new Acces
+                {
+                    Id = (int)al[i],
+                    Nom = (string)al[i + 1],
+                    LesPiscines = piscines
+                });
+            }
+            return resultat;
+        }
+    }
+}
--- dev/SolutionPiscine/Piscine_DAL/Repository.cs
+++ dev/SolutionPiscine/Piscine_DAL/Repository.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +9,16 @@ namespace Piscine_DAL
 {
     public static class Repository
     {
+        private static OccupationContext Context = new OccupationContext();
+        public static void Enregistrer(ArrayList alPiscines, ArrayList alAccess)
+        {
+            Context.LesPiscines.AddRange(alPiscines.ToListPiscine());
+            Context.LesAcces.AddRange(alAccess.ToListAcces());
+        }
+
+        public static List<Piscine> GetAllPiscines()
+        {
+            throw new NotImplementedException();
+        }
     }
 }
