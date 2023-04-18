3745494fca1f2c6363f3a8f9894e4c9fea01c6c3
Ali MAKRI <github@makri.fr>
2023-04-18 10:30:06 +0200

    10:29

--- dev/DesignPatterns/P_AsyncAvecDelegue/Delegue.cs
+++ dev/DesignPatterns/P_AsyncAvecDelegue/Delegue.cs
@@ -9,13 +9,27 @@ namespace P_AsyncAvecDelegue
 {
     public class Tache
     {
-        public void Execution(string message, int temps)
+        public Personne P;
+        public void Execution(string nom, string message, int temps)
         {
+            if (P == null) P = new Personne { Nom = nom };
             Thread.Sleep(1000 * temps);
             Console.ForegroundColor = ConsoleColor.Green;
             Console.WriteLine("Exécution de la tâche {0}", message);
             Console.ForegroundColor = ConsoleColor.Cyan;
         }
+        public void RetourExecution(IAsyncResult r)
+        {
+            string message = r.AsyncState as string;
+
+            Console.ForegroundColor = ConsoleColor.Magenta;
+            Console.WriteLine("{0} : {1}", message, DateTime.Now);
+            Console.ForegroundColor = ConsoleColor.Cyan;
+        }
     }
-    delegate void TacheDelegate(string a, int b);
+    delegate void TacheDelegate(string a, string b, int c);
+    public class Personne
+    {
+        public string Nom;
+    }
 }
--- dev/DesignPatterns/P_AsyncAvecDelegue/Program.cs
+++ dev/DesignPatterns/P_AsyncAvecDelegue/Program.cs
@@ -13,9 +13,11 @@ namespace P_AsyncAvecDelegue
             Console.ForegroundColor = ConsoleColor.Cyan;
             var tache1 = new Tache();
             var tache2 = new Tache();
+            var tache3 = new Tache();
 
             Console.WriteLine(DateTime.Now);
 
+            #region Versions
             // Version 1
             //tache1.Execution("Tâche 1", 5);
             //tache2.Execution("Tâche 2", 3);
@@ -25,14 +27,31 @@ namespace P_AsyncAvecDelegue
             //var dTache2 = new TacheDelegate(tache2.Execution);
             //dTache1.Invoke("Tâche 1", 5);
             //dTache1.Invoke("Tâche 2", 3);
+            #endregion
 
             // Version 3
+            //var dTache1 = new TacheDelegate(tache1.Execution);
+            //var dTache2 = new TacheDelegate(tache2.Execution);
+            //var dTache3 = new TacheDelegate(tache3.Execution);
+
+            //var dRetourTache1 = new AsyncCallback(tache1.RetourExecution);
+            //var dRetourTache2 = new AsyncCallback(tache2.RetourExecution);
+
+            //dTache1.BeginInvoke("T1", "Tâche 1", 5, dRetourTache1, "Retour tâche 1");
+            //dTache1.BeginInvoke("T2", "Tâche 2", 3, dRetourTache2, "Retour tâche 2");
+            //dTache3("T3", "Tâche 2", 3);
+            //Console.WriteLine(tache3.P.Nom); // Processus inter-thread sont interdits
+            //Console.WriteLine(DateTime.Now);
+
+            // Version 3bis
             var dTache1 = new TacheDelegate(tache1.Execution);
             var dTache2 = new TacheDelegate(tache2.Execution);
-            dTache1.BeginInvoke("Tâche 1", 5, null, null);
-            dTache1.Invoke("Tâche 2", 3);
+            var dTache3 = new TacheDelegate(tache3.Execution);
 
-
+            dTache1.BeginInvoke("T1", "Tâche 1", 5, tache1.RetourExecution, "Retour tâche 1");
+            dTache1.BeginInvoke("T2", "Tâche 2", 3, tache2.RetourExecution, "Retour tâche 2");
+            dTache3("T3", "Tâche 2", 3);
+            Console.WriteLine(tache3.P.Nom); // Processus inter-thread sont interdits
             Console.WriteLine(DateTime.Now);
 
             Console.ReadLine();
