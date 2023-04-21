31ca56df11fb4995bbf9300b5c4d35a293781d97
Ali MAKRI <github@makri.fr>
2023-04-21 11:29:52 +0200

    Vresion 1

---
+++ dev/Communication/A_InterfaceWCF/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace A_InterfaceWCF
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string s = "Ali Makri";
+            string t = s.ToUpper();
+            Console.WriteLine(t);
+            Console.ReadLine();
+        }
+    }
+}
