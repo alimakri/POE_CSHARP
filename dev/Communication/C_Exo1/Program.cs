45f10ba7eaeca39cac4420c6a30228cf7f6b7c98
Ali MAKRI <github@makri.fr>
2023-04-24 10:59:43 +0200

    10:59

---
+++ dev/Communication/C_Exo1/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C_Exo1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //
+
+            //
+
+            // Ne pas changer
+            Console.WriteLine("Liste des pays parlant français");
+            foreach(var pays in liste)
+            {
+                Console.WriteLine(pays);
+            }
+        }
+    }
+}
