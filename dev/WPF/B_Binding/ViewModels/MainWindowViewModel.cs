4d4afd224fa3e947472bda3ebe3d66c452c59e0a
Ali MAKRI <github@makri.fr>
2023-04-28 12:05:50 +0200

    12:05

--- dev/WPF/B_Binding/ViewModels/MainWindowViewModel.cs
+++ dev/WPF/B_Binding/ViewModels/MainWindowViewModel.cs
@@ -8,7 +8,19 @@ namespace B_Binding.ViewModels
 {
     public class MainWindowViewModel
     {
-        public string Fleur1 { get; set; } = "Lila de mai";
-        public string Fleur2 { get; set; } = "Rose du printemps";
+        public Fleur Fleur1 { get; set; }
+        public Fleur Fleur2 { get; set; }
+        public MainWindowViewModel()
+        {
+            Fleur1 = new Fleur { Id = 1, Nom = "Rose du printemps", Couleur = "Red", Image = "/images/rose.png" };
+            Fleur2 = new Fleur { Id = 2, Nom = "Lila de mai", Couleur = "Purple", Image = "/images/lila.png" };
+        }
+    }
+    public class Fleur
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
+        public string Couleur { get; set; }
+        public string Image { get; set; }
     }
 }
