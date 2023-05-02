1f9f7428cbb2808ec32ecd64cfb3a388148faf58
Ali MAKRI <github@makri.fr>
2023-05-02 16:13:01 +0200

    16:12

---
+++ dev/WPF/F_Binding/MyData.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace F_Binding
+{
+    public class MyData
+    {
+        public MyData()
+        {
+            ColorName = "Green";
+            Description = "Button background color";
+            ForeColorName = "Orange";
+        }
+        public string ColorName { get; set; }
+        public string Description { get; set; }
+        public string ForeColorName { get; set; }
+    }
+}
---
+++ dev/WPF/G_CollectionBindings/Data/Customer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_CollectionBindings.Data
+{
+    public class Customer
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public Guid CustomerID { get; set; }
+        public int Age { get; set; }
+    }
+    public class CustomerCollection : List<Customer>
+    {
+        public CustomerCollection()
+        {
+        }
+    }
+}
