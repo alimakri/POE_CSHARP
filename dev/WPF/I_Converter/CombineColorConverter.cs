ebb1810fdb5b8786b0845369ba191a2e085bed95
Ali MAKRI <github@makri.fr>
2023-05-03 14:19:12 +0200

    14:19

---
+++ dev/WPF/I_Converter/CombineColorConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace I_Converter
+{
+    public class CombineColorConverter : IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            return new SolidColorBrush(Color.FromArgb(255, (byte)values[0], (byte)values[1], (byte)values[2]));
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
