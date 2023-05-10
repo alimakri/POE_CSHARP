1734cc39532ea6240f390f052a2897a1d5dd4174
Ali MAKRI <github@makri.fr>
2023-05-10 16:30:02 +0200

    16:30

---
+++ dev/HtmlCssJs/J_XmlHttp_/Controllers/DataController.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace J_XmlHttp_.Controllers
+{
+    public class DataController : ApiController
+    {
+        public string Get() { return "hello"; }
+        public string Post() { return JsonConvert.SerializeObject(new Personne { Nom="MAKRI", Prenom="Ali" }); }
+    }
+    public class Personne { public string Nom { get; set; } public string Prenom { get; set; } }
+}
---
+++ dev/HtmlCssJs/J_XmlHttp_/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="J_XmlHttp_.Global" Language="C#" %>
