dce618019ada31f74cf12670db1c6e9bf225daa8
Ali MAKRI <github@makri.fr>
2023-05-11 12:30:03 +0200

    12:29

---
+++ dev/HtmlCssJs/K_Lab05/Scripts/pages/register.js
@@ -0,0 +1,36 @@
+﻿// Get the registration <form> element from the DOM.
+var form = document.getElementById("registration-form");
+var submitButton = form.querySelector("button");
+    
+// TODO: Task 1 - Get the password <input> elements from the DOM by ID
+var passwordInput = document.getElementById("password");
+var confirmPasswordInput = document.getElementById("confirm-password");
+
+function checkPasswords() {
+    // TODO: Task 2 - Compare passwordInput value to confirmPasswordInput value
+    var passwordsMatch = passwordInput.value === confirmPasswordInput.value;
+    // TODO: Task 3 - If passwords don't match then display error message on confirmPasswordInput (using setCustomValidity)
+    //                If passwords do match then clear the error message (setCustomValidity with empty string)
+    if (passwordsMatch) confirmPasswordInput.setCustomValidity("");
+    else confirmPasswordInput.setCustomValidity("Your passwords don't match. Please type the same password again.");
+    
+};
+
+var addPasswordInputEventListeners = function () {
+    // TODO: Task 4 - Listen for the "input" event on passwordInput and confirmPasswordInput.
+    //       Call the checkPasswords function
+    passwordInput.addEventListener("input", checkPasswords, false);
+    confirmPasswordInput.addEventListener("input", checkPasswords, false);
+};
+
+var formSubmissionAttempted = function() {
+    form.classList.add("submission-attempted");
+};
+
+var addSubmitClickEventListener = function() {
+    submitButton.addEventListener("click", formSubmissionAttempted, false);
+};
+
+addPasswordInputEventListeners();
+addSubmitClickEventListener();
+
---
+++ dev/HtmlCssJs/K_Lab05/Scripts/pages/schedule.js
@@ -0,0 +1,89 @@
+﻿var schedule = [];
+var list = document.getElementById("schedule");
+var track1CheckBox = document.getElementById("show-track-1");
+var track2CheckBox = document.getElementById("show-track-2");
+
+// TODO: Create a function called downloadSchedule
+//       Use an XMLHttpRequest to GET "/schedule/list"
+//       The response will be a JSON object of the form "{ schedule: [ ... ] }"
+//       Save the array into the schedule variable
+//       Then call displaySchedule()
+function downloadSchedule() {
+    var request = new XMLHttpRequest();
+    var url = "http://localhost:59813/schedule/list";
+    request.open("GET", url);
+    request.onreadystatechange = function () {
+        if (request.readyState === 4 && request.status === 200) {
+            try {
+                schedule = JSON.parse(request.responseText);
+                displaySchedule();
+            }
+            catch (exception) { alert("Erreur récup schedule"); }
+        }
+    }
+    request.send();
+}
+function createSessionElement(session) {
+    var li = document.createElement("li");
+
+    li.sessionId = session.id;
+
+    var star = document.createElement("a");
+    star.setAttribute("href", "#");
+    star.setAttribute("class", "star");
+    li.appendChild(star);
+
+    var title = document.createElement("span");
+    title.textContent = session.title;
+    li.appendChild(title);
+
+    return li;
+};
+
+function clearList() {
+    while (list.firstChild) {
+        list.removeChild(list.firstChild);
+    }
+}
+
+function displaySchedule() {
+    clearList();
+    for (var i = 0; i < schedule.length; i++) {
+        var tracks = schedule[i].tracks;
+        var isCurrentTrack = (track1CheckBox.checked && tracks.indexOf(1) >= 0) ||
+            (track2CheckBox.checked && tracks.indexOf(2) >= 0);
+        if (isCurrentTrack) {
+            var li = createSessionElement(schedule[i]);
+            list.appendChild(li);
+        }
+    }
+}
+
+function saveStar(sessionId, isStarred) {
+    // TODO: Create an XMLHttpRequest that POSTs to "/schedule/star/{sessionId}"
+    //       The request body must have the content type "application/x-www-form-urlencoded"
+    //       e.g. "starred=true" or "starred=false"
+    //       The response contains a JSON object "{ starCount: <number> }"
+    //       If the star count is more than 50, warn the user about this being a busy session.
+}
+
+function handleListClick(event) {
+    var isStarElement = event.srcElement.classList.contains("star");
+    if (isStarElement) {
+        event.preventDefault(); // Stop the browser following the clicked <a> element's href.
+
+        var listItem = event.srcElement.parentNode;
+        if (listItem.classList.contains("starred")) {
+            listItem.classList.remove("starred");
+            saveStar(listItem.sessionId, false);
+        } else {
+            listItem.classList.add("starred");
+            saveStar(listItem.sessionId, true);
+        }
+    }
+}
+
+track1CheckBox.addEventListener("click", displaySchedule, false);
+track2CheckBox.addEventListener("click", displaySchedule, false);
+list.addEventListener("click", handleListClick, false);
+
