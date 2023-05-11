48f5680b9b24fb9187ff60188b88578b41d60504
Ali MAKRI <github@makri.fr>
2023-05-11 08:47:11 +0200

    08:47

---
+++ dev/SolutionPiscine/H_UILWeb/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="H_UILWeb.MvcApplication" Language="C#" %>
---
+++ dev/SolutionPiscine/H_UILWeb/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(H_UILWeb.Startup))]
+namespace H_UILWeb
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
