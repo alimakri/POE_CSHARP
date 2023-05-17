4baae25b57df428aa1937d400a1030ab5b23c5cc
Ali MAKRI <github@makri.fr>
2023-05-17 16:01:21 +0200

    16:01

--- dev/ASPNetMVC_frameworkNet/C_Todo_V1/Controllers/TodoController.cs
+++ dev/ASPNetMVC_frameworkNet/C_Todo_V1/Controllers/TodoController.cs
@@ -9,14 +9,27 @@ namespace C_Todo_V1.Controllers
 {
     public class TodoController : Controller
     {
-        public ActionResult Index()
-        {
-            return View(new List<Todo>
+        private List<Todo> Todoes = new List<Todo>
             {
                 new Todo{Id=1, Libelle="Acheter du pain", Fait=false},
                 new Todo{Id=2, Libelle="Réviser HTML5", Fait=true},
                 new Todo{Id=3, Libelle="Prendre 4 jours de Week-end", Fait=false},
-            });
+            };
+        public ActionResult Index()
+        {
+            return View(Todoes);
+        }
+        public ActionResult Edit(int id)
+        {
+            var todo = Todoes.FirstOrDefault(x => x.Id == id);
+            if (todo == null) return RedirectToAction("Index");
+            return View(todo);
+        }
+        public ActionResult Delete(int id)
+        {
+            var todo = Todoes.FirstOrDefault(x => x.Id == id);
+            if (todo != null) Todoes.Remove(todo);
+            return RedirectToAction("Index");
         }
     }
 }
