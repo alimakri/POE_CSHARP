0325bf44f5fc5c669afee96a088d144ae29187cd
Ali MAKRI <github@makri.fr>
2023-05-22 10:39:22 +0200

    10:39

--- dev/ASPNetMVC_frameworkNet/C_Todo_V1/Models/Todo.cs
+++ dev/ASPNetMVC_frameworkNet/C_Todo_V1/Models/Todo.cs
@@ -1,6 +1,7 @@
 ﻿using C_Todo_V1.Data;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity;
 using System.Linq;
 using System.Web;
 
@@ -11,5 +12,13 @@ namespace C_Todo_V1.Models
         public int Id { get; set; }
         public string Libelle { get; set; }
         public bool Fait { get; set; }
+    }
+    public class TodoContext : DbContext
+    {
+        public TodoContext() :base("name=TodoConfig")
+        {
+
+        }
+        public DbSet<Todo> Todoes { get; set; }
     }
 }
