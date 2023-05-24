faae44ddaa67f15d2a73b02de11aae35a3030579
Ali MAKRI <github@makri.fr>
2023-05-24 10:16:23 +0200

    10:16

--- dev/ASPNetMVC_frameworkNet/Q_Repository/Data/Repository.cs
+++ dev/ASPNetMVC_frameworkNet/Q_Repository/Data/Repository.cs
@@ -1,5 +1,7 @@
-﻿using System;
+﻿using Q_Repository.Models;
+using System;
 using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 
@@ -7,5 +9,11 @@ namespace Q_Repository.Data
 {
     public class Repository
     {
+        internal List<Categorie> GetAllCat()
+        {
+            var cnx = new SqlConnection();
+            cnx.ConnectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=AdventureWorks2017;Integrated Security=True";
+            cnx.Open();
+        }
     }
 }
