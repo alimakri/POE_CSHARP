8c462658864fbfca24549024e806be09bbdfdb5b
Ali MAKRI <github@makri.fr>
2023-05-24 12:30:41 +0200

    12:30

--- dev/ASPNetMVC_frameworkNet/Q_Repository/Controllers/DataController.cs
+++ dev/ASPNetMVC_frameworkNet/Q_Repository/Controllers/DataController.cs
@@ -12,14 +12,10 @@ namespace Q_Repository.Controllers
     public class DataController : ApiController
     {
         private Repository Repo = new Repository();
-        public string PostSousCat(int id)
+        public string GetSousCat(int id)
         {
             var sousCats = Repo.GetSousCats(id);
             return JsonConvert.SerializeObject(sousCats);
-        }
-        public string Get()
-        {
-            return "Hello";
         }
     }
 }
