dd0a1e76ada5562df93fff06b1a1aa2a7950f3b7
Ali MAKRI <github@makri.fr>
2023-05-26 10:45:08 +0200

    10:45

--- dev/ASPNetMVC_frameworkNet/Y_Session/Controllers/HomeController.cs
+++ dev/ASPNetMVC_frameworkNet/Y_Session/Controllers/HomeController.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Caching;
 using System.Web.Mvc;
 using System.Web.SessionState;
 using Y_Session.Models;
@@ -11,28 +12,51 @@ namespace Y_Session.Controllers
     public class HomeController : Controller
     {
         private List<PanierElement> Panier = null;
-        private HttpSessionState MaSession;
+        //private HttpSessionState MaSession;
         public HomeController()
         {
-            MaSession = System.Web.HttpContext.Current.Session;
+            //MaSession = System.Web.HttpContext.Current.Session;
         }
         public ActionResult Index()
         {
-            ViewBag.SessionId = MaSession.SessionID;
-            return View();
+            ViewBag.SessionId = Session.SessionID;
+            return View(new List<PanierElement>());
         }
         [HttpPost]
         public ActionResult Index(PanierElement commande)
         {
-            ViewBag.SessionId = MaSession.SessionID;
+            ViewBag.SessionId = Session.SessionID;
 
             // Récupération du panier dans Session
             Panier = (List<PanierElement>)Session["Panier"];
+            if (Panier == null) Panier = new List<PanierElement>();
+
             // Ajoût de la commande
             Panier.Add(commande);
-            // Enregistrement du panier dans Session
+            // Conservation du panier dans Session
             Session["Panier"] = Panier;
             return View(Panier);
+        }
+        public ActionResult Admin()
+        {
+            List<string> liste=null;
+            if (HttpRuntime.Cache["ListeProduit"] == null)
+            {
+                liste = new List<string>
+                {
+                    "Savon","Shampoing","Dentifrice"
+                };
+                HttpRuntime.Cache.Add(
+                    "ListeProduit",
+                    liste,
+                    null,
+                    DateTime.Now.AddMinutes(10),
+                    Cache.NoSlidingExpiration,
+                    CacheItemPriority.High,
+                    null);
+            }
+            liste = (List<string>) HttpRuntime.Cache["ListeProduit"];
+            return View(liste);  
         }
     }
 }
