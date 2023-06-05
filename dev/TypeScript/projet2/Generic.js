aef2880f24906563262b3b1a7128949f36674377
Ali MAKRI <github@makri.fr>
2023-06-05 12:43:32 +0200

    12:43

--- dev/TypeScript/projet2/Generic.js
+++ dev/TypeScript/projet2/Generic.js
@@ -1,3 +1,18 @@
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
 // Fonction générique
 function AfficherV1(s) { console.log(s); }
 var n = 1;
@@ -17,3 +32,38 @@ var Affichage = /** @class */ (function () {
 console.log('------------------');
 var aff = new Affichage();
 aff.AfficherV3(n + 3);
+// aff.AfficherV3(texte);
+var Personne1 = /** @class */ (function () {
+    function Personne1(nom) {
+        this.nom = nom;
+    }
+    return Personne1;
+}());
+var Employe = /** @class */ (function (_super) {
+    __extends(Employe, _super);
+    function Employe() {
+        return _super !== null && _super.apply(this, arguments) || this;
+    }
+    return Employe;
+}(Personne1));
+var Identite = /** @class */ (function () {
+    function Identite() {
+    }
+    Identite.prototype.AfficherV4 = function (p) { console.log(p.nom); };
+    return Identite;
+}());
+var Animal1 = /** @class */ (function () {
+    function Animal1(nom) {
+        this.nom = nom;
+    }
+    return Animal1;
+}());
+var pAlain = new Personne1('Alain');
+var identite1 = new Identite();
+identite1.AfficherV4(pAlain);
+var pVero = new Employe('Vero');
+var identite2 = new Identite();
+identite1.AfficherV4(pVero);
+var pMilou = new Animal1('Milou');
+var identite3 = new Identite();
+identite3.AfficherV4(pMilou);
--- dev/TypeScript/projet2/Generic.ts
+++ dev/TypeScript/projet2/Generic.ts
@@ -17,3 +17,46 @@ class Affichage<T>{
 console.log('------------------');
 let aff = new Affichage<number>();
 aff.AfficherV3(n+3);
+// aff.AfficherV3(texte);
+
+class Personne1 {
+    nom:string;
+    constructor(nom:string){this.nom=nom;}
+}
+class Employe extends Personne1{}
+class Identite<T extends Personne1>{
+    AfficherV4(p:T):void{console.log(p.nom); }
+}
+class Animal1 {
+    nom:string;
+    constructor(nom:string){this.nom=nom;}
+}
+class Animal2 {
+    surNom:string;
+    constructor(nom:string){this.surNom=nom;}
+}
+class Animal3 {
+    nom:string;
+    age:number=0;
+    constructor(nom:string){this.nom=nom;}
+}
+
+let pAlain = new Personne1('Alain');
+let identite1 = new Identite<Personne1>();
+identite1.AfficherV4(pAlain);
+
+let pVero = new Employe('Vero');
+let identite2 = new Identite<Personne1>();
+identite1.AfficherV4(pVero);
+
+let pMilou = new Animal1('Milou');
+let identite3 = new Identite<Animal1>(); // même structure
+identite3.AfficherV4(pMilou);
+
+// let pIdefix = new Animal1('Idefix');
+// let identite4 = new Identite<Animal2>(); // structure différente
+// identite4.AfficherV4(pIdefix);
+
+let pRintintin = new Animal3('Rintintin');
+let identite4 = new Identite<Animal3>(); // structure différente
+identite4.AfficherV4(pRintintin);
