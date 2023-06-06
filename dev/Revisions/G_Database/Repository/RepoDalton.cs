a28daadb088d0bf20b123f1b4d0a714b84da8aaa
Ali MAKRI <github@makri.fr>
2023-06-06 15:24:11 +0200

    15:24

---
+++ dev/Revisions/G_Database/Repository/RepoDalton.cs
@@ -0,0 +1,23 @@
+﻿using G_Database.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace G_Database.Repository
+{
+    public class RepoDalton
+    {
+        public List<Personne> Get()
+        {
+            return new List<Personne>
+            {
+                    new Personne { Nom = "Dalton", Prenom = "Joe" },
+                    new Personne { Nom = "Dalton", Prenom = "Jack" },
+                    new Personne { Nom = "Dalton", Prenom = "William" },
+                    new Personne { Nom = "Dalton", Prenom = "Averel" },
+            };
+        }
+    }
+}
