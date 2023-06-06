5f8f8bf7fcbd90e1a12f75bf461502ae9643eb31
Ali MAKRI <github@makri.fr>
2023-06-06 10:41:21 +0200

    10:41

--- dev/TypeScript/projet2/Revisions/ADONET/Program.cs
+++ dev/TypeScript/projet2/Revisions/ADONET/Program.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ADONET
 {
@@ -14,7 +16,7 @@ namespace ADONET
                 Console.WriteLine($"{i}. {cat}");
                 i++;
             }
-            var catChoisi = FaireUnChoix();
+            var catChoisi = FaireUnChoix(cats.ToList());
             foreach (var produit in repo.GetProduits(catChoisi))
             {
                 Console.WriteLine(produit);
@@ -24,6 +26,23 @@ namespace ADONET
             // id nomDuProduit Prix 
             // id nomDuProduit Prix 
             Console.ReadLine();
+        }
+
+        private static string FaireUnChoix(List<string> cats)
+        {
+            Console.ForegroundColor = ConsoleColor.Cyan;
+            Console.Write("Choisissez une catégorie (1-4): ");
+            
+            var saisieStr = Console.ReadLine();
+            int saisie = 0;
+            while (!int.TryParse(saisieStr, out saisie) || saisie < 1 || saisie > 4)
+            {
+                saisieStr = Console.ReadLine();
+                Console.Write("Choisissez une catégorie (1-4): ");
+            }
+
+            Console.ForegroundColor = ConsoleColor.Gray;
+            return cats[saisie-1];
         }
     }
 }
