5d926dcb0cae669a387f56d4fc5f73caf7b6f408
Ali MAKRI <github@makri.fr>
2023-06-07 12:29:55 +0200

    12:29

---
+++ dev/Revisions/L_Cirque/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace L_Cirque
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ISpectacle seance1 = new SeanceCinema();
+            seance1.VoirAcrobaties();
+            seance1.VoirAnimaux();
+            Console.ReadLine();
+        }
+    }
+
+    public interface ISpectacle
+    {
+        void VoirAcrobaties();
+        void VoirAnimaux();
+    }
+
+    public class Cirque : ISpectacle
+    {
+        public void VoirAcrobaties() { Console.WriteLine("Je vois des acrobaties au cirque"); }
+        public void VoirAnimaux() { Console.WriteLine("Je vois des animaux au cirque"); }
+        public void ParticiperAvecLesClowns() { Console.WriteLine("Je participe avec les clowns"); }
+
+    }
+    public class SeanceCinema : ISpectacle
+    {
+        public void VoirAcrobaties() { Console.WriteLine("Je vois des acrobaties au cinéma"); }
+        public void VoirAnimaux() { Console.WriteLine("Je vois des animaux au cinéma"); }
+    }
+}
---
+++ dev/Revisions/M_Attribut/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace M_Attribut
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var jardin = new Jardinage();
+            jardin.TaillerHaies();
+        }
+    }
+    class Jardinage
+    {
+        public static string Outil = "";
+        [Outil]
+        public void TaillerHaies()
+        {
+            if (Outil == "TailleHaie") Console.WriteLine("C'est bon !");   
+        }
+    }
+}
