cb9d0855e96c41602978591eb1f4471f916fb5f7
Ali MAKRI <github@makri.fr>
2023-06-08 12:06:42 +0200

    12:06

--- dev/Revisions/Q_Chifoumi/MainWindowViewModel.cs
+++ dev/Revisions/Q_Chifoumi/MainWindowViewModel.cs
@@ -8,19 +8,102 @@ using System.Windows.Media;
 
 namespace Q_Chifoumi
 {
-    public class MainWindowViewModel
+    enum Chifoumi { Pierre = 1, Feuille = 2, Ciseau = 3 }
+    public class MainWindowViewModel : ViewModelBase
     {
+        #region Constructor
+        public MainWindowViewModel()
+        {
+            PierreCommand = new RelayCommand(PierreCommandExecute);
+            FeuilleCommand = new RelayCommand(FeuilleCommandExecute);
+            CiseauCommand = new RelayCommand(CiseauCommandExecute);
+        }
+        #endregion
+
+        #region Propriétés
+        private Random alea = new Random();
+        #endregion
+
+        #region Command
+        private void PierreCommandExecute(object obj)
+        {
+            ChoixUtilisateurExecute(Chifoumi.Pierre);
+        }
+        private void FeuilleCommandExecute(object obj)
+        {
+            ChoixUtilisateurExecute(Chifoumi.Feuille);
+        }
+        private void CiseauCommandExecute(object obj)
+        {
+            ChoixUtilisateurExecute(Chifoumi.Ciseau);
+        }
+
+        private void ChoixUtilisateurExecute(Chifoumi choixUtilisateur)
+        {
+            var choixMachine = ChoixMachineExecute();
+            ChoixMachine = new ChoixViewModel
+            {
+                Image = $"/images/{choixMachine.ToString()}.png",
+                Texte = choixMachine.ToString()
+            };
+            ComparerChoix(choixUtilisateur, choixMachine);
+        }
+
+        private void ComparerChoix(Chifoumi choixU, Chifoumi choixM)
+        {
+            if (choixM == choixU)
+            {
+                Points = 0;
+            }
+            else if (
+                choixU == Chifoumi.Pierre && choixM == Chifoumi.Ciseau ||
+                choixU == Chifoumi.Feuille && choixM == Chifoumi.Pierre ||
+                choixU == Chifoumi.Ciseau && choixM == Chifoumi.Feuille
+                )
+            {
+                Points++;
+            }
+            else
+            {
+                Points = 0;
+            }
+        }
+
+        private Chifoumi ChoixMachineExecute()
+        {
+            var choixInt = alea.Next(1, 4);
+            return (Chifoumi)choixInt;
+        }
+        #endregion
+
         #region Bindings
         public ICommand PierreCommand { get; set; }
         public ICommand FeuilleCommand { get; set; }
         public ICommand CiseauCommand { get; set; }
-        public int Points { get; set; }
-        public string Message { get; set; }
+        public int Points
+        {
+            get { return points; }
+            set { points = value; OnPropertyChanged("Points"); }
+        }
+        private int points;
+        public string Message
+        {
+            get { return message; }
+            set { message = value; OnPropertyChanged("Message"); }
+        }
+        private string message;
         public string MessageCouleur { get; set; }
-        public ChoixViewModel ChoixMachine { get; set; }
+        //public ChoixViewModel ChoixMachine { get; set; }
+
+        public ChoixViewModel ChoixMachine
+        {
+            get { return choixMachine; }
+            set { choixMachine = value; OnPropertyChanged("ChoixMachine"); }
+        }
+        private ChoixViewModel choixMachine;
         #endregion
     }
-    public class ChoixViewModel
+    public class ChoixViewModel 
     {
         public string Image { get; set; }
         public string Texte { get; set; }
--- dev/Revisions/Q_Chifoumi/RelayCommand.cs
+++ dev/Revisions/Q_Chifoumi/RelayCommand.cs
@@ -6,7 +6,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Input;
 
-namespace Installation.CommunViewModels
+namespace Q_Chifoumi
 {
     public class RelayCommand : ICommand
     {
