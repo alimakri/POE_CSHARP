246ac045af8882be383a85f668c70d58dec7742c
Ali MAKRI <github@makri.fr>
2023-06-13 11:34:08 +0200

    11:34

--- dev/Revisions/Z2_Metier/Personnes.cs
+++ dev/Revisions/Z2_Metier/Personnes.cs
@@ -4,7 +4,7 @@ using System.Data;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-using Z3_Donnees;
+using Z5_Donnees;
 using Z4_Dto;
 
 namespace Z2_Metier
--- dev/Revisions/Z5_Donnees/Repository.cs
+++ dev/Revisions/Z5_Donnees/Repository.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Z4_Dto;
 
 namespace Z5_Donnees
 {
     public class Repository
     {
+        internal DataSet2 GetPersonnes()
+        {
+            var ds = new DataSet2();
+
+        }
     }
 }
