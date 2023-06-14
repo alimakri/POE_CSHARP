52fc25c5b23bf849b671d73532b9a1c4cbf292ee
Ali MAKRI <github@makri.fr>
2023-06-14 12:31:15 +0200

    12:31

--- dev/Revisions/Z6_Bureau/MainWindowViewModel.cs
+++ dev/Revisions/Z6_Bureau/MainWindowViewModel.cs
@@ -18,6 +18,7 @@ namespace Z6_Bureau
 
             ListePersonne = new List<PersonneViewModel>();
             ListeDetails = new List<DetailsViewModel>();
+            ListeMagasins = new List<MagasinViewModel>();
             foreach (DataRow row in ds1.Tables["Personne"].Rows)
             {
                 ListePersonne.Add(new PersonneViewModel { Id = (int)row["Id"], NomComplet = (string)row["NomComplet"] });
@@ -36,11 +37,15 @@ namespace Z6_Bureau
             {
                 currentPersonne = value;
                 var ds1 = Personnes.GetDetails(SourceEnum.Real, currentPersonne.Id);
+
+
+
                 ListeDetails.Clear();
                 foreach (DataRow row in ds1.Tables["Detail"].Rows)
                 {
                     ListeDetails.Add(new DetailsViewModel { Magasin = (string)row["Magasin"] });
                 }
+                ListeMagasins = ListeDetails.Distinct().Select(x => new MagasinViewModel { Nom = x.Magasin }).ToList();
             }
         }
         private PersonneViewModel currentPersonne;
@@ -59,15 +64,40 @@ namespace Z6_Bureau
             }
         }
         private List<DetailsViewModel> listeDetails;
+        public List<MagasinViewModel> ListeMagasins
+        {
+            get
+            {
+                return listeMagasins;
+            }
+            set
+            {
+                listeMagasins = value;
+                OnPropertyChanged("ListeMagasins");
+            }
+        }
+        private List<MagasinViewModel> listeMagasins;
 
+    }
+    public class MagasinViewModel
+    {
+        public int Id { get; set; }
+        public string Nom { get; set; }
     }
     public class PersonneViewModel
     {
         public int Id { get; set; }
         public string NomComplet { get; set; }
     }
-    public class DetailsViewModel
+    public class DetailsViewModel : ViewModelBase
     {
-        public string Magasin { get; set; }
+
+        public string Magasin
+        {
+            get { return magasin; }
+            set { magasin = value; OnPropertyChanged("Magasin"); }
+        }
+        private string magasin;
+
     }
 }
